79a22273c91f2fde52ba302e8da51dc09d1b0950
vlad <miclaus.da.vlad@student.utcluj.ro>
2026-02-10 01:46:30 +0200

    changed theme for nvim and kitty to gruvbox-material

---
+++ dot_config/exact_nvim/exact_lua/exact_plugins/empty_colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_better_performance = 1
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+}
