b495106cd149077f45bc19f7eda2320ec7f07f81
psychedeliquew <254417711+psychedeliquew@users.noreply.github.com>
2026-02-11 15:22:20 +0200

    run_once_after_install-configs.sh to install configs after everything

---
+++ run_once_after_install-configs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+# greetd config
+sudo mv ~/.config/greetd /etc/
+sudo systemctl enable greetd
+
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+
+# zsh-syntax-highlighting
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+        "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+fi
+
+# zsh-autosuggestions
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git \
+        "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+fi
+
+# changing shell to zsh
+chsh -s /bin/zsh
+
+# reboot
+reboot
