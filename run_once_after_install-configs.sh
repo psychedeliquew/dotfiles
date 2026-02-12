20eb198131fd3ec2a81bfce1c7fd8a5eedc64f65
psychedeliquew <254417711+psychedeliquew@users.noreply.github.com>
2026-02-12 10:10:45 +0200

    add ufw config

--- run_once_after_install-configs.sh
+++ run_once_after_install-configs.sh
@@ -7,6 +7,7 @@ if [ -d "$HOME/.config/greetd" ]; then
     sudo systemctl enable greetd
 fi
 
+# variable for zsh
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 
 # zsh-syntax-highlighting
@@ -20,6 +21,12 @@ if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions.git \
         "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 fi
+
+# ufw config
+sudo ufw reset
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw enable
 
 # changing shell to zsh
 chsh -s /bin/zsh
